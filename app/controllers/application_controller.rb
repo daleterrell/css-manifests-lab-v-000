af0433cfc220f859387de94eb9824dc4f190f2f8
Dale Terrell <tragicid@gmail.com>
2019-07-17 14:53:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   /*
-*= require 
+*= require stylesheets/blogs
+*= require stylesheets/pages
+*= require stylesheets/posts
 */
   protect_from_forgery with: :exception
 end
